e1f53946ff7134744c93e2b6642b81fb0883b486
Junsung Cho <junsung@directional.net>
2024-07-21 10:22:35 +0900

    fix: use timestamp for sort

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -5,7 +5,7 @@ export async function load(event: RequestEvent): Promise<{
     blocks: ApiResponse<PagedModel<Block>>;
     txs: ApiResponse<PagedModel<Transaction>>
 }> {
-    const blocks: ApiResponse<PagedModel<Block>> = await callApi(event, 'GET', `/v1/blocks?size=18&sort=id,desc`);
-    const txs: ApiResponse<PagedModel<Transaction>> = await callApi(event, 'GET', `/v1/transactions?size=18&sort=block.id,desc`);
+    const blocks: ApiResponse<PagedModel<Block>> = await callApi(event, 'GET', `/v1/blocks?size=18&sort=timestamp,desc`);
+    const txs: ApiResponse<PagedModel<Transaction>> = await callApi(event, 'GET', `/v1/transactions?size=18&sort=timestamp,desc`);
     return {blocks, txs}
 }
--- src/routes/tx/+page.server.ts
+++ src/routes/tx/+page.server.ts
@@ -2,5 +2,5 @@ import type {RequestEvent} from "@sveltejs/kit";
 import {callApi} from "$lib/server/api";
 
 export async function load(event: RequestEvent): Promise<ApiResponse<PagedModel<Transaction>>> {
-    return await callApi(event, 'GET', `/v1/transactions?sort=block.Id,desc&` + event.url.searchParams.toString())
+    return await callApi(event, 'GET', `/v1/transactions?sort=timestamp,desc&` + event.url.searchParams.toString())
 }
